505824f73feeaaa1c4e38c7ffd35a81253e8187a
Rohan Godha <yoyorodha@gmail.com>
2023-07-27 21:30:07 +0530

    MongoDB Added

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,21 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+var _dotenv = _interopRequireDefault(require("dotenv"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 // const databaseConnection = require("./database/connection");
 // Database connection
 // import ConnectDB from "./database/connection";
-
-dotenv.config();
-
-const zomato = express();
-
-zomato.use(express.json());
-
+_dotenv.default.config();
+const zomato = (0, _express.default)();
+zomato.use(_express.default.json());
 zomato.get("/", (req, res) => {
   res.json({
-    message: "Server is running",
+    message: "Server is running"
   });
 });
-
 const PORT = 4000;
 
 // zomato.listen(PORT, () => {
